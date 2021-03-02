9b7a2dd5df0dd2f7602d094ce6a72ac731333dba
GoswamiTushar <tushargoswami1510@gmail.com>
2021-03-02 23:36:56 +0530

    Responsive Nav Bar

--- scripts/homejs.js
+++ scripts/homejs.js
@@ -26,3 +26,14 @@ function initSlideShow(slideshow) {
 
   }, time);
 }
+
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navigation-bar');
+navItems = document.querySelector('.nav-items');
+
+
+burger.addEventListener('click', ()=>{
+  navItems.classList.toggle('v-class-resp');
+  navbar.classList.toggle('h-nav-resp');
+
+})
